84fb0464d9b5be73924d136c3a64e86705941b27
Sylvain Janet <sylvain.janet@vidal.fr>
2026-09-17 01:17:00 +0200

    🐛 ESLint allows out http adapters to have responsibility in name
    Closes #1.

--- frontend/eslint/architecture/file-expectations.js
+++ frontend/eslint/architecture/file-expectations.js
@@ -109,12 +109,12 @@ const fileExpectations = {
     ],
     httpContracts: [
       expectedFile(
-        ['<scope>-http-contract'],
+        ['<scope>-http-contract', '<scope>-<responsibility>-http-contract'],
         TYPESCRIPT_EXTENSIONS,
         declarations.contextHttpContract,
       ),
       expectedFile(
-        ['<scope>-http-contract.mapper'],
+        ['<scope>-http-contract.mapper', '<scope>-<responsibility>-http-contract.mapper'],
         TYPESCRIPT_EXTENSIONS,
         declarations.singleClass,
       ),
